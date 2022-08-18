4abc0e686f087a011dcaf825589f4deaf1ef571a
tkurskiev <tkurskiev@gmail.com>
2022-08-18 12:17:00 +0300

    Scaffolded dbContext using EF

---
+++ BookStore.API/Data/BookStoreDbContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using BookStore.API.Models;
+
+namespace BookStore.API.Data
+{
+    public partial class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext()
+        {
+        }
+
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.;Database=BookStoreDb;User Id=Tims;Password=LightningBolt1;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasIndex(e => new { e.Author, e.Title }, "book_ak_1")
+                    .IsUnique();
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Author)
+                    .HasMaxLength(256)
+                    .IsUnicode(false)
+                    .HasColumnName("author")
+                    .IsFixedLength();
+
+                entity.Property(e => e.PublicationDate)
+                    .HasColumnType("date")
+                    .HasColumnName("publication_date");
+
+                entity.Property(e => e.Title)
+                    .HasMaxLength(256)
+                    .IsUnicode(false)
+                    .HasColumnName("title")
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ BookStore.API/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookStore.API.Models
+{
+    public partial class Book
+    {
+        public int Id { get; set; }
+        public string Author { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public DateTime PublicationDate { get; set; }
+    }
+}
