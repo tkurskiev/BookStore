526a5340560ab189cff614db2c463310f2e8b448
tkurskiev <tkurskiev@gmail.com>
2022-08-18 12:28:46 +0300

    Added controller

---
+++ BookStore.API/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        public BooksController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
